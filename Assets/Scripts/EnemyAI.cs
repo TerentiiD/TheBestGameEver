d994ba242b0489524e336482b4a53e8c54f84382
TerentiiD <terentijdanilov@gmail.com>
2024-03-02 19:04:38 +0300

    1

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> patrolPoints;
+    public PlayerController player;
+    public float viewAngle;
+
+    private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+
+    private void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        var direcrion = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direcrion) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direcrion, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+        if (!_isPlayerNoticed)
+        {
+            if (_isPlayerNoticed)
+            {
+                _navMeshAgent.destination = player.transform.position;
+            }
+        }
+        PatrolUpdate();
+    }
+
+    private void PatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoint();
+        }
+    }
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+}
